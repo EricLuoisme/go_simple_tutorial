3d94a7ac728ee60a7757de4c5644a0d161834b92
roylic <lykismekk@outlook.com>
2023-01-23 15:50:11 +0800

    editing for first assignment on Coursera

--- hello/go.mod
+++ hello/go.mod
@@ -1,10 +1,3 @@
 module example.com/hello
 
-go 1.15
-
-replace example.com/greetings => ../greetings
-
-require (
-	example.com/greetings v0.0.0-00010101000000-000000000000
-	golang.org/x/example v0.0.0-20220412213650-2e68773dfca0 // indirect
-)
+go 1.19
--- hello/hello.go
+++ hello/hello.go
@@ -1,27 +1,10 @@
 package main
 
 import (
-	"example.com/greetings"
 	"fmt"
-	"golang.org/x/example/stringutil"
-	"log"
 )
 
 func main() {
-
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
-
-	names := []string{"Gladys", "Samantha", "Darrin"}
-
-	messages, err := greetings.Hellos(names)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(messages)
-
-	// test for calling with another api
-	fmt.Println(stringutil.Reverse("Hello"))
+	sentence := "Hello,world!"
+	fmt.Println(sentence)
 }
