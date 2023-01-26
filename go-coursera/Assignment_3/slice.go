bb0fc0842e64239915a5ecd9d664f8d23531b046
roylic <lykismekk@outlook.com>
2023-01-26 12:26:02 +0800

    correct the assignment by following requirement -> create an empty integer slice of size (empty! but with Size)

--- go-coursera/Assignment_3/slice.go
+++ go-coursera/Assignment_3/slice.go
@@ -8,12 +8,12 @@ import (
 )
 
 func main() {
-	// init by make
-	theSlice := make([]int, 3)
+	// init by make, empty slice but with size 3
+	theSlice := make([]int, 0, 3)
 
 	// traverse the slice
-	curLen := 0
 	for {
+
 		// 1. scanning input
 		var inputStr string
 		fmt.Println("input new integer or 'X' for exit")
@@ -21,22 +21,19 @@ func main() {
 		if err != nil {
 			return
 		}
+
 		// 2. exit the loop if match "X"
 		if strings.Compare("X", inputStr) == 0 {
 			break
 		}
+
 		// 3. adding into slice
 		atoi, _ := strconv.Atoi(inputStr)
-		if curLen >= 3 {
-			theSlice = append(theSlice, atoi)
-		} else {
-			theSlice[curLen] = atoi
-		}
-		curLen++
-	}
+		theSlice = append(theSlice, atoi)
 
-	// sorting -> under two lines both working
-	sort.Ints(theSlice)
-	//sort.Slice(theSlice, func(i, j int) bool { return theSlice[i] < theSlice[j] })
-	fmt.Println(theSlice)
+		// 4. sorting -> under two lines both working
+		sort.Ints(theSlice)
+		//sort.Slice(theSlice, func(i, j int) bool { return theSlice[i] < theSlice[j] })
+		fmt.Println(theSlice)
+	}
 }
