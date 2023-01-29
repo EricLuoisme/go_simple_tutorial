18f844f7510582ad699d2ab06c2896a9b8193a20
roylic <lykismekk@outlook.com>
2023-01-29 22:52:07 +0800

    adding basic data type -> call by value & call by reference comparison demo

---
+++ go-coursera/passByV_passByR/BasicDataType.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 0, 0
+
+	fmt.Println("\n## Init")
+	fmt.Printf("Memory Location a: %p, b: %p\n", &a, &b)
+	fmt.Printf("Value a:%d, b:%d\n", a, b)
+
+	Add(a)
+
+	AddRef(&b)
+
+	fmt.Println("\n## Final")
+	fmt.Printf("Memory Location a: %p, b: %p\n", &a, &b)
+	fmt.Printf("Value a:%d, b:%d\n", a, b)
+
+}
+
+func Add(x int) {
+	fmt.Println("\n## 'Add' Function")
+	fmt.Printf("Before Add, Memory Location: %p, Value: %d\n", &x, x)
+	x++
+	fmt.Printf("After Add, Memory Location: %p, Value: %d\n", &x, x)
+}
+
+func AddRef(x *int) {
+	fmt.Println("\n## 'Add By Reference' Function")
+	fmt.Printf("Before Add Ref, Memory Location: %p, Value: %d\n", x, *x)
+	*x++ // * will call the value of a pointer
+	fmt.Printf("After Add Ref, Memory Location: %p, Value: %d\n", x, *x)
+}
