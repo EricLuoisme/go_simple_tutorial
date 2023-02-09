adfd4a0954b933438b8cfc39bf715c95c2bcdab2
roylic <lykismekk@outlook.com>
2023-02-09 10:18:50 +0800

    fulfilling the explanation of the race-condition

--- go-coursera/Assignment_10/concurrency.go
+++ go-coursera/Assignment_10/concurrency.go
@@ -9,17 +9,29 @@ import (
 Explanation:
 
 Here is an example about race-condition that might happen and causing problems.
-Whole program is about switching a string variable to 'A' 10,000 times and switch
-it to 'B' another 10,000 times. sync.WaitGroup is used for let the function wait
-till other tasks are finished. According to the order of the code, it should just
-set to 'A' first (in a period), then result in variable == 'B'. Now, Two goroutines
-is created for handling this two tasks separately.
+Whole program is about summing up two number and get the result. But instead of
+adding the whole number, the number is adding 1 each times, then reach the result
+e.g. 10000 -> for loop 10000 times and add 1 each loop. Two number inputs are set
+15000 in the program. Now, Two goroutines is created for handling this summing task
+separately, and verify any race-condition happens. Main procedure was loop 10 times
+for better observation.
 
-But as you can see in the main function, we find out variable resulting in 'A' in some
-case (in my computer I go 8/10 times of 'A'), which is different from the result that
-only execute task in single goroutine
+As you could see after running the program, the result could hardly get 30,000,
+(In my computer, I never reach it), most the result of summation would between
+[15,000, 30,000).
 
-This is because, two goroutines manipulating the same variable, we
+The reason why this happens, is because the two goroutines are in race-condition.
+The Function addingOneByOne, which the goroutines calling, manipulate same global
+variable 'summation'. In computer, program runs by COPYING data from the memory
+to the CPU cache, then letting the CPU execute it, and finally copying the result
+back to the memory for the program to use. In multi-core CPU, each core would execute
+task independently. So in this case, two goroutines are considered as different tasks.
+For a calling to Function addingOneByOne, OS would help COPY the value 'summation'.
+If these two goroutines just 'run at the same time', then OS would COPY value TWICE
+into two different CPU core, then inside this two cpu, val would +1 for each. After
+summation, OS would COPY both value back to same slot, which means it +1 +1, but only
+resulting in +1. That's the race-condition and why we only get result that less than
+correct answer of the summation.
 
 */
 
