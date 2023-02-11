1ae43007baa710aa1b38791c80cc8e060435d74e
roylic <lykismekk@outlook.com>
2023-02-11 16:19:18 +0800

    adding printing for each splited task

--- go-coursera/Assignment_11/SortArrayGoroutine.go
+++ go-coursera/Assignment_11/SortArrayGoroutine.go
@@ -20,7 +20,7 @@ func main() {
 
 	// 2. split task
 	cn := make(chan []int, 4)
-	each := len(intSlice) / 4
+	each := len(strSlice) / 4
 	go splitSort(intSlice[:each], cn)
 	go splitSort(intSlice[each:each*2], cn)
 	go splitSort(intSlice[each*2:each*3], cn)
@@ -45,6 +45,7 @@ func wholeSorted(numOfParts int, cn chan []int) []int {
 
 // splitSort is simple sorting and signify to then channel after finished
 func splitSort(part []int, cn chan []int) {
+	fmt.Printf("split slice for this task: %v\n", part)
 	sort.Ints(part)
 	cn <- part
 }
@@ -79,7 +80,7 @@ func merge2Slice(s1, s2 []int) []int {
 // readSplitInputLine reading whole input line and split them into string slice
 func readSplitInputLine() []string {
 	// scan
-	fmt.Println("Please entry integer which split by space, enter for finished. e.g. 5 15 7 22 9 1 3 2 6 4 8")
+	fmt.Println("Please entry integer which split by space, enter for finished. e.g. 5 15 13 7 22 9 1 3 2 6 4 8")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	inputLine := scanner.Text()
