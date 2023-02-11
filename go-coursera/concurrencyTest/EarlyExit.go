60c04368232fddc79a7aebb9c50e21d651eaa076
roylic <lykismekk@outlook.com>
2023-02-11 14:26:23 +0800

    using waiting group for solving the early exit problem

--- go-coursera/concurrencyTest/EarlyExit.go
+++ go-coursera/concurrencyTest/EarlyExit.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
+func foo(wg *sync.WaitGroup) {
+	fmt.Println("New routine")
+	wg.Done()
+}
+
 func main() {
-	// almost main finished before the new goroutine started
-	go fmt.Println("New routine")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go foo(&wg)
+	wg.Wait()
 	fmt.Println("Main routine")
 }
